3c96ea9f895b9ab9332fe8d35008fb5145e689f1
nurselaltin <nurselaltin.na@gmail.com>
2024-04-13 01:50:42 +0300

    added models and repository of product.

---
+++ API/Elasticsearch.API/Elasticsearch.API/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Nest;
+
+namespace Elasticsearch.API.Models
+{
+    //Hız önemli ise => NOSQL
+    public class Product
+    {
+        [PropertyName("_id")]
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public ProductFeature? Feature { get; set; } // Sanal ilişki
+
+    }
+}
