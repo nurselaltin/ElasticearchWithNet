f1794c7eb7b14c9c75abfaf284518028ef6d8502
nurselaltin <nurselaltin.na@gmail.com>
2024-04-13 00:56:40 +0300

    added elasticsearch connection.

---
+++ API/Elasticsearch.API/Elasticsearch.API/Extensions/Elasticsearch.cs
@@ -0,0 +1,16 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace Elasticsearch.API.Extensions
+{
+    public static class Elasticsearch
+    {
+        public static void AddElastic(this IServiceCollection services, IConfiguration configuration)
+        {
+            var pool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("Elastic")["Url"]!)); //Neden tek node bağlandık ????
+            var settings = new ConnectionSettings(pool);
+            var client = new ElasticClient(settings);
+            services.AddSingleton(client);
+        }
+    }
+}
